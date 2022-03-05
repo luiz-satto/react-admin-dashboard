16887bd91bdd43bdd81b8f9786238f0be0debaee
Luiz Satto <luizf.satto@gmail.com>
2022-03-05 14:38:34 -0300

    Added Sidebar

--- react-admin-dashboard-spa/src/components/Layout/index.tsx
+++ react-admin-dashboard-spa/src/components/Layout/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import Sidebar from '../Sidebar'
+import Routes from '../../routes'
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
 const Layout: React.FC = () => {
   return (
-    <div>
-        Hello Layout
-    </div>
+    <BrowserRouter>
+      <Route render={(props) => (
+        <div className='layout'>
+          <Sidebar {...props} />
+          <div className="layout__content">
+            <div className="layout__content-main">
+              <Routes />
+            </div>
+          </div>
+        </div>
+      )}></Route>
+    </BrowserRouter>
   )
 }
 
---
+++ react-admin-dashboard-spa/src/components/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+const Sidebar: React.FC<RouteComponentProps> = () => {
+  return (
+    <div>
+        Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
